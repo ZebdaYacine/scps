d36bc23aa83865202939ccee59612ebcb992afaa
ZebdaYacine <36519629+ZebdaYacine@users.noreply.github.com>
2024-11-15 15:18:04 +0100

    deply version

--- frontend/app/lib/core/secret/sercret.dart
+++ frontend/app/lib/core/secret/sercret.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: non_constant_identifier_names
 
 class Secret {
-  static String URL_API = "http://192.168.71.60:3000";
+  // static String URL_API = "http://192.168.71.60:3000";
+  static String URL_API = "https://scps-api.onrender.com";
   static String aesKey = "my32charSecretKeyMustBe32chars!!";
 }
